45ed45d0b792b7d4f5110c63e5d35fb9207bd7e6
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-09-06 22:21:16 -0300

    Se crea backend para cliente Web

---
+++ BffBancoWeb/src/main/java/com/duoc/BffBancoWeb/BffBancoWebApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.BffBancoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffBancoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffBancoWebApplication.class, args);
+	}
+
+}
---
+++ BffBancoWeb/src/test/java/com/duoc/BffBancoWeb/BffBancoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.BffBancoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffBancoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
